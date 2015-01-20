a41fcafebc590ebdeacd2162854c43390bb967bc
Draško Kokić <drasko.kokic@quartett-mobile.de>
2015-01-20 19:07:43 +0100

    add extra activity to show the NotSerializableException when exiting activity by the solution with Presenter and Callback objects

---
+++ MyApp3/app/src/main/java/net/kodra/myapp3/OtherActivity.java
@@ -0,0 +1,13 @@
+package net.kodra.myapp3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
